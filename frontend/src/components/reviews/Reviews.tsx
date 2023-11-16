555217caa36aa5322b1ef6e4745203405b0da9b7
Aviral3004 <86399898+Aviral3004@users.noreply.github.com>
2023-11-16 16:48:43 +0530

    changes

--- frontend/src/components/reviews/Reviews.tsx
+++ frontend/src/components/reviews/Reviews.tsx
@@ -4,9 +4,10 @@ import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
 import { useParams } from "react-router-dom";
 
-
 // import Gridsplit from "../homepage/GridSplit";
 import NotUserReviewsButton from "./NotUserReviewsButton";
+// import { TextField } from "@mui/material";
+import PopUpButton from "./PopUpButton";
 
 const Reviews = () => {
   const location = useParams();
@@ -32,22 +33,7 @@ const Reviews = () => {
       </Typography>
       <Grid container py={5} justifyContent="center">
         <Grid item xs={12} sm={8} md={9} lg={10} mt={2}>
-          <Paper elevation={3} sx={{ mx: 10, bgcolor: "primary.main", px: 3 }}>
-            <Typography py={4} pl={0.4} textAlign="left" variant="subtitle2">
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt
-              fuga quisquam molestias officia ab consectetur maxime nemo sed eum
-              rem deserunt sequi explicabo veritatis incidunt debitis odio,
-              reprehenderit ad nisi, fugit ipsam tempora magnam! Quos vero odio
-              cupiditate ratione. Dolorem magni aperiam adipisci dolorum
-              reprehenderit sint, quam explicabo recusandae. Numquam repellendus
-              quas natus. Nemo fugit nobis fuga debitis eius beatae iure error
-              id. Sed, tempora corrupti? Asperiores cum facilis distinctio quae?
-              Nisi, tempore voluptas. Iusto, magnam! Deleniti molestias,
-              suscipit repudiandae fugiat voluptate earum perferendis similique
-              voluptates. Quaerat sunt vero, earum in, atque porro consectetur
-              at perferendis reiciendis natus placeat id.
-            </Typography>
-          </Paper>
+          <PopUpButton />
         </Grid>
       </Grid>
       <Typography
