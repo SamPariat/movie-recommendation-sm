2bb2eac3b5d62c698867460ec6b1f8bc094f1ac6
Sam <apunthelegend@outlook.com>
2023-12-07 14:35:02 +0530

    Added new cors origin

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,11 @@ app.use((req: Request, res: Response, next: NextFunction) => {
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:3000"],
+    origin: [
+      "http://localhost:5173",
+      "http://localhost:3000",
+      "http://127.0.0.1:5173",
+    ],
     credentials: true,
     preflightContinue: true,
   })
