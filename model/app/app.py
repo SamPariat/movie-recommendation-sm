7356f970c259511193fa35dc1c9ec6b66d5a8093
Sam <apunthelegend@outlook.com>
2023-12-27 16:05:05 +0530

    Updated flask server base route

--- model/app/app.py
+++ model/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from .routes.recommendation_bp import recommendation_blueprint
 from .routes.sentiment_bp import sentiment_blueprint
 
@@ -6,6 +6,11 @@ app = Flask(__name__)
 
 app.register_blueprint(recommendation_blueprint)
 app.register_blueprint(sentiment_blueprint)
+
+@app.route("/", methods=["GET"])
+def app_run():
+    return jsonify({'message': 'Flask server on'})
+
 
 # if __name__ == '__main__':
 #     print('BTP Flask backend running on port 3524')
